5f30cecafcda60713fd7996c2709b62adbaedb2c
Ajay Kumar M <ajaykumarm.be23@uceou.edu>
2023-09-13 23:55:25 +0530

    auth router

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express')
+const express = require('express');
 const connectToMongo = require('./dbConnect')
-const app = express()
+const app = express();
+const auth = require('./routes/auth');
 const port = 3000
 
+app.use(express.json());
+
 connectToMongo();
-app.get('/', (req, res) => {
-  res.send('Hello Ajay')
-})
+app.use('/api/auth', auth);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
